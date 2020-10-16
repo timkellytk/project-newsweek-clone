c04caa9d640651c2cfbea0bdb21341a7baa920ee
Tim Kelly <timpkelly1@gmail.com>
2020-10-16 16:09:48 +1100

    Updated the navbar to include an avatar icon on small devices for the navbar

--- src/components/Navigation/NavBtns/LoginBtn.js
+++ src/components/Navigation/NavBtns/LoginBtn.js
@@ -2,7 +2,16 @@ import React from 'react';
 
 const LoginBtn = () => (
   <a className="nav-btn" href="#empty">
-    Login
+    <span class="hidden sm:inline-block">Login</span>
+    <span class="sm:hidden">
+      <svg
+        class="fill-current text-gray-300 h-6 w-6"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+      >
+        <path d="M5 5a5 5 0 0 1 10 0v2A5 5 0 0 1 5 7V5zM0 16.68A19.9 19.9 0 0 1 10 14c3.64 0 7.06.97 10 2.68V20H0v-3.32z" />
+      </svg>
+    </span>
   </a>
 );
 
--- src/components/Navigation/NavFixed/NaxFixed.js
+++ src/components/Navigation/NavFixed/NaxFixed.js
@@ -6,7 +6,7 @@ const NavFixed = () => {
   return (
     <div class="flex justify-between items-center fixed top-0 left-0 right-0 bg-primary text-white px-3 py-2">
       <div class="font-logo text-2xl">Newsweek</div>
-      <div class="">
+      <div class="flex items-center">
         <LoginBtn />
         <SubscribeBtn />
       </div>
