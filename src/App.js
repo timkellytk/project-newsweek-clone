c95c6366831884df3a709dba716f6717908cf055
Tim Kelly <timpkelly1@gmail.com>
2020-10-19 15:29:47 +1100

    Fixed up the layout for the news section

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="bg-gray-200">
-      <div className="container lg:mx-auto bg-white">
+    <div className="xl:bg-gray-200">
+      <div className="xl:container mx-auto bg-white">
         <Navigation />
         <News />
         <Footer />
--- src/components/News/News.js
+++ src/components/News/News.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const News = () => {
-  return <div className="py-64 text-center">News</div>;
+  return (
+    <div className="">
+      <div className="news-grid">
+        <div className="h-64 bg-red-500">hello</div>
+        <div className="h-64 bg-teal-500"></div>
+        <div className="h-64 bg-orange-500"></div>
+      </div>
+    </div>
+  );
 };
 
 export default News;
